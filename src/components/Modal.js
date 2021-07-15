c898cfc0bc38ddd015844f8ad2085a221ce406d4
k1999-darouich <k.darouich_etu@enset-media.ac.ma>
2021-07-15 17:47:14 +0100

    AddModalComponent

---
+++ src/components/Modal.js
@@ -0,0 +1,12 @@
+function Modal(props) {
+
+    return (
+        <div className='modal'>
+            <h5>Are u sure?</h5>
+            <button className='btn btn--alt'>Cancel</button>
+            <button className='btn'>Confirm</button>
+        </div>
+    );
+}
+
+export default Modal;
